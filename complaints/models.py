7848ebd7ca247f4624ee9c8dd434bfe4e688ec25
Kudrat0777 <114284502+AnnSkachkova@users.noreply.github.com>
2024-03-11 19:24:16 +0300

    модели созданы

--- complaints/models.py
+++ complaints/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Complaint(models.Model):
+    user_id = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=100, default='new')
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
+
+
+class Response(models.Model):
+    complaint = models.ForeignKey(Complaint, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
